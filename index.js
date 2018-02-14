fcaf9a8f17d56da7ef4dd64b285a0c7005a47174
Robert Reinhard <robert.reinhard@bukwild.com>
2018-02-14 10:01:48 -0800

    Look for stylus rather than styl files

--- index.js
+++ index.js
@@ -3,24 +3,26 @@ module.exports = function nuxtSassResourcesLoader (moduleOptions = {}) {
         moduleOptions = {resources: moduleOptions}
     }
 
-    const options = Object.assign({}, {resources: this.options.sassResources}, moduleOptions)    
+    const options = Object.assign({}, {resources: this.options.sassResources}, moduleOptions)
 
     const sassResourcesLoader = {
         loader: 'sass-resources-loader', options
     }
 
     this.extendBuild((config, { isClient, isServer }) => {
+
         const sassLoader = config.module.rules.filter(({test}) => {
-            return ['/\\.sass$/', '/\\.scss$/'].indexOf(test.toString()) !== -1
+            return [ '/\\.styl/' ].indexOf(test.toString()) !== -1
         })
+
         const vueLoader = config.module.rules.find(({test}) => {
             return test.toString() === '/\\.vue$/'
         })
-    
+
         const loaders = vueLoader.options.loaders;
 
         Object.keys(loaders).forEach(loader => {
-            if (['sass', 'scss'].indexOf(loader) !== -1) {
+            if (['styl', 'stylus'].indexOf(loader) !== -1) {
                 loaders[loader].push(sassResourcesLoader)
             }
         })
@@ -31,5 +33,5 @@ module.exports = function nuxtSassResourcesLoader (moduleOptions = {}) {
 
     })
 }
-  
+
 module.exports.meta = require('./package.json')
